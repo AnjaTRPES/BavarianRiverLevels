0ee0ef1d1ba36dde241119c5848557d2c375e62b
AnjaTRPES <roeder.anja.sci@gmail.com>
2021-08-01 00:04:36 +0200

    A function to get the data of a particular station from the website

---
+++ scraper.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 31 15:58:57 2021
+
+@author: anja
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def get_river_stats(Region,Station, days):
+    """
+    Download the rivers water levels at a particular station over x days
+    region: str, Region where the river is found, ex. isar for the loisach river
+    station: measuring station with an unique identifier unfortunately e.g.
+        garmisch-o-d-partnachmuendung-16401006
+    days: int, number of days to be looked at
+    
+    returns:
+        Dataframe with datetime, level, hours
+    """
+    html='https://www.hnd.bayern.de/pegel/'+Region+'/'
+    html+=Station+'/tabelle?methode=wasserstand&days='+str(days)
+    dfs=pd.read_html(html)
+    river_stats=dfs[0]
+    river_stats[river_stats.columns[0]]=pd.to_datetime(river_stats[river_stats.columns[0]],
+           format='%d.%m.%Y %H:%M')
+    river_stats['hours']=(river_stats[river_stats.columns[0]]-river_stats[river_stats.columns[0]][0])/np.timedelta64(1,'h')
+    return river_stats
+
+
+if __name__=='__main__':
+    river_stats=get_river_stats('isar','garmisch-o-d-partnachmuendung-16401006',360)
+    plt.figure()
+    plt.plot(river_stats['hours'],river_stats[river_stats.columns[1]],'o-')
+    #plt.plot(river_stats['hours'])
+    plt.show()
+
+
+
